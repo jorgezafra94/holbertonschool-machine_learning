d64aeeb8f8b574571e434356f7db104747e8d297
jorgezafra94 <947@holbertonschool.com>
2020-04-13 21:55:18 +0000

    [feature] addition of two vectors

---
+++ math/0x00-linear_algebra/4-line_up.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+addition of two vectors
+"""
+
+
+def add_arrays(arr1, arr2):
+    if (len(arr1) == len(arr2)):
+        result = []
+        for i in range(len(arr1)):
+            result.append(arr1[i] + arr2[i])
+        return result
+    else:
+        return None

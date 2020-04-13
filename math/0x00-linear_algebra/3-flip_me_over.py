e62549dd7bce5e4d1b589e282d097be0efaf36c4
jorgezafra94 <947@holbertonschool.com>
2020-04-13 21:37:55 +0000

    [feature] transpose of matrix

---
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+transpose of a matrix
+"""
+
+
+def matrix_transpose(matrix):
+    new_matrix = []
+
+    try:
+        for i in range(len(matrix[0])):
+            new_matrix.append([])
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                new_matrix[j].append(matrix[i][j])
+    except (IndexError, TypeError):
+        for i in range(len(matrix)):
+            new_matrix.append([])
+        for i in range(len(matrix)):
+            new_matrix[i].append(matrix[i])
+    return new_matrix

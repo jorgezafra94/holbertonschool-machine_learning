4ab16fab2875ced642bec537f1eba62fe183f717
jorgezafra94 <947@holbertonschool.com>
2020-04-15 12:02:56 +0000

    [feature] addition of two vectors

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -5,6 +5,13 @@ addition of two vectors
 
 
 def add_arrays(arr1, arr2):
+    """
+    if both arrays have the same size you can add them
+    @arr1: first array
+    @arr2: second array
+    Return: - new array: this new array is the result of the addition
+            - None: if the addition cant be realized
+    """
     if (len(arr1) == len(arr2)):
         result = []
         for i in range(len(arr1)):

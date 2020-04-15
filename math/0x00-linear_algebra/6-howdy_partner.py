30535b3d6df196dbf0f7855fe28ecfacb7ae7ae0
jorgezafra94 <947@holbertonschool.com>
2020-04-15 12:05:22 +0000

    [feature] concatenation of two arrays

--- math/0x00-linear_algebra/6-howdy_partner.py
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -5,5 +5,11 @@ concatenate two arrays
 
 
 def cat_arrays(arr1, arr2):
+    """
+    concatenate two arrays
+    @arr1: array1
+    @arr2: array2
+    Return: the concatenation of these two arrays
+    """
     result = arr1 + arr2
     return result

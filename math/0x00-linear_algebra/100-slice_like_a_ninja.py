3411609156f83755551ac8c43edab60d03a659ab
jorgezafra94 <947@holbertonschool.com>
2020-04-15 12:20:35 +0000

    [feature] slice a matrix using Slice python function

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -8,6 +8,13 @@ import numpy as np
 
 
 def np_slice(matrix, axes={}):
+    """
+    slice is a python function that works as [:3] or this filters
+    slice(None, None, None)--- is the same as complete array
+    @matrix: matrix to filter or slice
+    @axes: dictionario that contains the information about what axis and filter should be done in matrix
+    Return: - new matrix: the matrix sliced
+    """
     listSlice = []
     for i in range(len(matrix.shape)):
         flag = 0

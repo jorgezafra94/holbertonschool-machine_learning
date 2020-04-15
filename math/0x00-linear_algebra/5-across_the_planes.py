f9581c80388a71b5d5ce70bd130888bfe46d7338
jorgezafra94 <947@holbertonschool.com>
2020-04-15 12:04:57 +0000

    [feature] matrix addition

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -5,6 +5,13 @@ addition of matrices
 
 
 def add_matrices2D(mat1, mat2):
+    """
+    this function adds two matrices with max 2D
+    @mat1: first matrix
+    @mat2: second matrix
+    Return: - new array: the result of the addition
+            - None: if the matrices cant be added
+    """
     result = []
     if ((len(mat1) == len(mat2)) and (len(mat1[0]) == len(mat2[0]))):
         for i in range(len(mat1)):

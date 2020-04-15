dfdf35cd6374ed2fe6803753ba4796e14e1c29d9
jorgezafra94 <947@holbertonschool.com>
2020-04-15 12:06:30 +0000

    [feature] normal multiplication of two 2d matrices

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -5,6 +5,14 @@ multiplication of two matrices
 
 
 def mat_mul(mat1, mat2):
+    """
+    normal multiplication of two matrices
+    @mat1: first matrix
+    @mat2: second matrix
+    Return: - new matrix: result of the multiplication
+            - None: if the matrices dont fulfill the
+                 condition to realize the multiplication
+    """
     new = []
     if (len(mat1[0]) == len(mat2)):
         for i in range(len(mat1)):

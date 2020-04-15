4a04d1cd18d9a0baacf5825a6c3600b47219246b
jorgezafra94 <947@holbertonschool.com>
2020-04-15 12:18:26 +0000

    [feature] wise-operators of Numpy

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-using wise-element operations of numpy
+using wise-element operations
 """
 
 
@@ -8,9 +8,13 @@ def np_elementwise(mat1, mat2):
     """
     using Numpy wise-element operators
     Methods of Numpy arrays or matrices
-    """
     suma = np.add(mat1, mat2)
     resta = np.subtract(mat1, mat2)
     multi = np.multiply(mat1, mat2)
     div = np.divide(mat1, mat2)
+    """
+    suma = mat1 + mat2
+    resta = mat1 - mat2
+    multi = mat1 * mat2
+    div = mat1 / mat2
     return (suma, resta, multi, div)

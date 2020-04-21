2baa41f664f086f426b55eae87f075b10e76a3bc
jorgezafra94 <947@holbertonschool.com>
2020-04-21 02:03:49 +0000

    [feature] 3d graphic using plasma color map colors behaviour

---
+++ math/0x01-plotting/101-pca.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+
+lib = np.load("pca.npz")
+data = lib["data"]
+labels = lib["labels"]
+
+data_means = np.mean(data, axis=0)
+norm_data = data - data_means
+_, _, Vh = np.linalg.svd(norm_data)
+pca_data = np.matmul(norm_data, Vh[:3].T)
+
+# create a 3d figure
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# labels
+ax.set_xlabel("U1")
+ax.set_ylabel("U2")
+ax.set_zlabel("U3")
+
+# graphic
+ax.scatter(xs=pca_data[:, 0], ys=pca_data[:, 1], zs=pca_data[:, 2],
+           c=labels, cmap=plt.cm.plasma)
+
+plt.title("PCA of Iris Dataset")
+plt.show()

6ef633344039e8ae77ef0fd554e14d0c5eeaf4c9
jorgezafra94 <947@holbertonschool.com>
2020-04-24 12:47:28 +0000

    [update] remove recursion

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,20 +4,7 @@ using seegma logic in order to add each result
 """
 
 
-def recursion(n, actual, value):
-    """
-    recursion: use seegma locig with function i^2
-    @n: top of seegma
-    @actual: actual value in seegma function
-    @value: actual result
-    Return: result of seegma function
-    """
-    if (n < actual):
-        return value
-    else:
-        value += (actual ** 2)
-        result = recursion(n, actual+1, value)
-    return result
+import numpy as np
 
 
 def summation_i_squared(n):
@@ -27,9 +14,6 @@ def summation_i_squared(n):
     @n: will be the top of seegma function
     Return: result of function seegma
     """
-    if not n:
-        return None
-    if (type(n) is not):
-        return None
-    result = recursion(n, 1, 0)
-    return result
+    result = list(map(lambda x: x**2, list(range(1, n+1))))
+    result = np.array(result)
+    return result.sum()

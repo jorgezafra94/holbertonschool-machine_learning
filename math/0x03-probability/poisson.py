c24c09cf12622277848e950b4d42fa2ab5ffdd94
jorgezafra94 <947@holbertonschool.com>
2020-04-28 23:06:09 +0000

    [feature] Poisson Class

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""
+Create our Poisson function
+"""
+
+
+class Poisson():
+    """
+    class Poisson
+    """
+    e = 2.7182818285
+
+    def __init__(self, data=None, lambtha=1.):
+        """
+        constructor
+        """
+
+        if data is None:
+            lambtha = float(lambtha)
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = lambtha
+
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            self.lambtha = sum(data)/len(data)
+
+    def pmf(self, k):
+        """
+        Probability Mass Function PMF
+        """
+        if type(k) is int or type(k) is float:
+            k = int(k)
+            if k < 0:
+                return 0
+            den = 1
+            for i in range(1, k+1):
+                den = den * i
+            PMF = ((Poisson.e ** (-self.lambtha)) * (self.lambtha ** k))/den
+            return PMF
+
+    def cdf(self, k):
+        """
+        Cumulative Distribution Function CDF
+        """
+        CDF = 0
+        for i in range(0, k+1):
+            CDF += self.pmf(i)
+        return CDF

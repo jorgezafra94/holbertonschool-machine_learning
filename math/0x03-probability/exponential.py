a2f9127e86669a07eabde53ea2ed292a88a58dec
jorgezafra94 <947@holbertonschool.com>
2020-04-28 23:05:52 +0000

    [feature] Exponential Class

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+Create our Exponential function
+"""
+
+
+class Exponential():
+    """
+    class Exponential
+    """
+    e = 2.7182818285
+
+    def __init__(self, data=None, lambtha=1.):
+        """
+        constructor
+        """
+        if data is None:
+            lambtha = float(lambtha)
+
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = lambtha
+
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            self.lambtha = 1/(sum(data)/len(data))
+
+    def pdf(self, x):
+        """
+        Probability Density Function PDF
+        """
+        if x < 0:
+            return 0
+        lam = self.lambtha
+        PDF = (lam * (Exponential.e ** (-lam * x)))
+        return PDF
+
+    def cdf(self, x):
+        """
+        Cumulative Distribution Function CDF
+        CDF = integral of pdf function
+        """
+        CDF = 1 - (Exponential.e ** (-self.lambtha * x))
+        return CDF

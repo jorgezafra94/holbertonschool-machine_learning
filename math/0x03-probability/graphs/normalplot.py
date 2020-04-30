17f63a6867c31228cfcc9e134a3568215c3c4689
jorgezafra94 <947@holbertonschool.com>
2020-04-30 21:06:12 +0000

    improving plotting

--- math/0x03-probability/graphs/normalplot.py
+++ math/0x03-probability/graphs/normalplot.py
@@ -1,16 +1,24 @@
-#!/usr/bin/python3
-
+#!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
 Normal = __import__('normal').Normal
 
+
 np.random.seed(0)
-data = np.random.normal(70, 10, 100).tolist()
-p1 = Normal(data)
+data = np.random.normal(30, 5, 1000).tolist()
+n = Normal(data)
 
-plt.hist(data, 10, density=True)
-x = np.arange(0, 100)
-y = [p1.pdf(i) for i in range(0, 100)]
+x = np.arange(0, 50, 0.001)
+y = [n.pdf(x) for x in x]
+z = [n.cdf(x) for x in x]
 
-plt.plot(x, y, c='r')
+fig = plt.figure()
+ax1 = plt.subplot2grid((3,1), (0,0))
+ax1.hist(data, 25, density=True)
+ax2 = plt.subplot2grid((3,1), (1,0))
+ax2.plot(x, y, c='r')
+ax2.set_xticks(np.arange(5, 50, 5))
+ax3 = plt.subplot2grid((3,1), (2,0))
+ax3.plot(x, z, c='g')
+ax3.set_ylim(-5, 5)
 plt.show()

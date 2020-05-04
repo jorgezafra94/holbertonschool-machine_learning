94b0be9b718e34444110cf9e50463dd0400031d2
jorgezafra94 <947@holbertonschool.com>
2020-05-04 13:03:00 +0000

    [feature] NN with private parameters

---
+++ supervised_learning/0x00-binary_classification/9-neural_network.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+"""
+neural network with one hidden layer performing binary classification:
+"""
+
+
+import numpy as np
+
+
+class NeuralNetwork():
+    """
+    Here is my first Holberton NeuralNetwork Class
+    here we are going to use One hidden layer
+    the main things to keep in mind about a neuron is
+    the ecuation y = sum(w.x) + b
+    where w are the weights in this case W
+          x are the inputs in this case nx
+          b are the biases
+    A is the activated output of the neuron
+    """
+    def __init__(self, nx, nodes):
+        """
+        constructor of class
+        nx is the number of input features to the neuron
+        nodes is the number of nodes found in the hidden layer
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        # input layer parameters
+        W1 = [np.random.randn(nx).tolist() for i in range(nodes)]
+        self.__W1 = np.array(W1)
+        b1 = [np.zeros(1).tolist() for i in range(nodes)]
+        self.__b1 = np.array(b1)
+        self.__A1 = 0
+        # hidden layer parameters
+        W2 = [np.random.randn(nodes).tolist() for i in range(1)]
+        self.__W2 = np.array(W2)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """
+        return values of Weights first layer
+        """
+        return self.__W1
+
+    @property
+    def W2(self):
+        """
+        return values of Weights hidden layer
+        """
+        return self.__W2
+
+    @property
+    def b1(self):
+        """
+        return values of bias first layer
+        """
+        return self.__b1
+
+    @property
+    def b2(self):
+        """
+        return values of bias hidden layer
+        """
+        return self.__b2
+
+    @property
+    def A1(self):
+        """
+        return values of Active hidden layer
+        """
+        return self.__A1
+
+    @property
+    def A2(self):
+        """
+        return values of Active output
+        """
+        return self.__A2

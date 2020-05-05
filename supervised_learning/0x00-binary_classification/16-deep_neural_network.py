f633fed3eabfb2538101e45d3546f6fdf4841f23
jorgezafra94 <947@holbertonschool.com>
2020-05-05 19:23:41 +0000

    [fix] adding len(layers) in Initializing DNN

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -23,6 +23,8 @@ class DeepNeuralNetwork():
             raise ValueError("nx must be a positive integer")
         if type(layers) is not list:
             raise TypeError("layers must be a list of positive integers")
+        if len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
         self.cache = {}

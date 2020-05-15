333351cbd8ef989bdcb89ca7537edadb468cc827
jorgezafra94 <947@holbertonschool.com>
2020-05-15 22:10:02 +0000

    [fe] placeholders with TensorFlow

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -17,6 +17,6 @@ def create_placeholders(nx, classes):
     y is the placeholder for the one-hot labels for the input data
     """
 
-    x = tf.placeholder(tf.float32, shape=(None, nx), name='x')
-    y = tf.placeholder(tf.float32, shape=(None, classes), name='y')
+    x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
+    y = tf.placeholder(tf.float32, shape=[None, classes], name='y')
     return (x, y)

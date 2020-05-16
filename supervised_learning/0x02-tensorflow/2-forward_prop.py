0877931cd87ae762fa78a7f0334ec663f4a1704a
jorgezafra94 <947@holbertonschool.com>
2020-05-16 13:56:45 +0000

    [fix] calling A with x as default i= 0 and removing tensor import

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""
-forward propagation tensorflow
+""" 
+Forward Propagation with TensorFlow
 """
 
 create_layer = __import__('1-create_layer').create_layer
@@ -9,17 +9,13 @@ create_layer = __import__('1-create_layer').create_layer
 def forward_prop(x, layer_sizes=[], activations=[]):
     """
     x is the placeholder for the input data
-    layer_sizes is a list containing the number
-          of nodes in each layer of the network
+    layer_sizes is a list containing the number of
+                nodes in each layer of the network
     activations is a list containing the activation
-           functions for each layer of the network
+             functions for each layer of the network
     Returns: the prediction of the network in tensor form
-    For this function, you should import your create_layer
-    function with create_layer = __import__('1-create_layer').create_layer
     """
     A = create_layer(x, layer_sizes[0], activations[0])
-    
-    for i in range(1, len(layer_sizes)):    
-        A = create_layer(A, layer_sizes[i], activations[i])
-
+    for i in range(1, len(layer_sizes)):
+        A = create_layer(net, layer_sizes[i], activations[i])
     return A

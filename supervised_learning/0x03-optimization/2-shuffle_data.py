962e9ca99b599ed463612362a93100d350a23c65
jorgezafra94 <947@holbertonschool.com>
2020-05-20 23:43:38 +0000

    [up] andrew logic shuffle

--- supervised_learning/0x03-optimization/2-shuffle_data.py
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -18,7 +18,6 @@ def shuffle_data(X, Y):
     Returns: the shuffled X and Y matrices
     """
     vector = np.random.permutation(np.arange(X.shape[0]))
-    X_shu, Y_shu = X, Y
-    X_shu[np.arange(X.shape[0])] = X[vector]
-    Y_shu[np.arange(Y.shape[0])] = Y[vector]
+    X_shu = X[vector]
+    Y_shu = Y[vector]
     return X_shu, Y_shu

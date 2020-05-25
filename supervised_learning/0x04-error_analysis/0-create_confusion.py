91003b2fc8e580e15b3dfc69a54d71a1b3890569
jorgezafra94 <947@holbertonschool.com>
2020-05-25 15:00:18 +0000

    [fe] confusion matrix

---
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+confusion Matrix or error Matrix
+"""
+
+
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """
+    * labels is a one-hot numpy.ndarray of shape (m, classes)
+               containing the correct labels for each data point
+    * m is the number of data points
+    * classes is the number of classes
+    * logits is a one-hot numpy.ndarray of shape (m, classes)
+               containing the predicted labels
+    * Returns: a confusion numpy.ndarray of shape (classes, classes) with row
+               indices representing the correct labels and column indices
+               representing the predicted labels
+    """
+    classes = labels.shape[1]
+    # Actual
+    actual = np.argmax(labels, axis=1)
+    # predicted
+    pred = np.argmax(logits, axis=1)
+    # confusion matrix initialization and creation
+    confusion = np.array(np.zeros(classes ** 2))
+    confusion = confusion.reshape(classes, classes)
+    # fulling the matrix initialization
+    for i in range(actual.size):
+        confusion[actual[i], pred[i]] += 1
+    return confusion

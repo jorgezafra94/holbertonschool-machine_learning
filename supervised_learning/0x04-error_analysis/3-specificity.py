aef237381a1cffbf84bd986951a24a12fd12339c
jorgezafra94 <947@holbertonschool.com>
2020-05-25 17:18:52 +0000

    [fe] specificity of confusion matrix

---
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Specificity confusion matrix
+"""
+
+
+def specificity(confusion):
+    """
+     * confusion is a confusion numpy.ndarray of shape (classes, classes) where
+              row indices represent the correct labels and column indices
+              represent the predicted labels
+    * classes is the number of classes
+    * Returns: a numpy.ndarray of shape (classes,) containing the specificity
+              of each class
+    """
+    truePositive = confusion.diagonal()
+    falsePositive = confusion.sum(axis=0) - truePositive
+    falseNegative = confusion.sum(axis=1) - truePositive
+    aux = truePositive + falsePositive + falseNegative
+    trueNegative = confusion.sum() - aux
+    specificity = trueNegative / (trueNegative + falsePositive)
+    return specificity

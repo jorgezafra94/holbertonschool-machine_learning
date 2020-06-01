6cece5e1ae093b4984549c1fcec7ecdf23a0dfa2
jorgezafra94 <947@holbertonschool.com>
2020-06-01 23:06:43 +0000

    [fe] test a model in keras

---
+++ supervised_learning/0x06-keras/12-test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Testings
+"""
+
+
+def test_model(network, data, labels, verbose=True):
+    """
+    network is the network model to test
+    data is the input data to test the model with
+    labels are the correct one-hot labels of data
+    verbose is a boolean that determines if output should be printed
+            during the testing process
+    Returns: the loss and accuracy of the model with the testing
+            data, respectively
+    """
+    result = network.evaluate(x=data, y=labels, verbose=verbose)
+    return result

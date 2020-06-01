e43aadf2fc74ef40ea60b003ee405a455cb7cfb2
jorgezafra94 <947@holbertonschool.com>
2020-06-01 16:26:09 +0000

    [fe] Applying Optimization Algo in a Model

---
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Applying Adam Optimizator
+"""
+
+
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """
+    * that sets up Adam optimization for a keras model with
+         categorical crossentropy loss and accuracy metrics:
+    * network is the model to optimize
+    * alpha is the learning rate
+    * beta1 is the first Adam optimization parameter
+    * beta2 is the second Adam optimization parameter
+    Returns: None
+    """
+    Adam = K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2)
+    network.compile(loss='categorical_crossentropy', optimizer=Adam)

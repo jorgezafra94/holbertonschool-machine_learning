513f4a9a6f6f98b265638ddebabe1313b13dcc2e
jorgezafra94 <947@holbertonschool.com>
2020-06-04 01:17:31 +0000

    [fe] applying padding and filter to images

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -16,29 +16,32 @@ def convolve_grayscale_same(images, kernel):
     Returns:
 
     """
-    HK, WK = kernel.shape
-
-    if (HK % 2 == 0):
-        pad_H = HK // 2
+    kh, kw = kernel.shape
+    # when we have to give a padding we have to calculate it like this
+    # if it is even just divide it bt 2 if it is odd subs 1 and divide by 2
+    if (kh % 2 == 0):
+        ph = kh // 2
     else:
-        pad_H = (HK - 1) // 2
-    if (WK % 2 == 0):
-        pad_W = WK // 2
+        ph = (kh - 1) // 2
+    if (kw % 2 == 0):
+        pw = kw // 2
     else:
-        pad_W = (WK - 1) // 2
+        pw = (kw - 1) // 2
 
+    # We have to apply the padding to the input images
     new_images = np.pad(images,
-                        ((0, 0), (pad_H, pad_H), (pad_W, pad_W)),
+                        ((0, 0), (ph, ph), (pw, pw)),
                         'constant')
-    m, HI, WI = images.shape
+    m, ih, iw = images.shape
 
-    new_H = (HI + (2 * pad_H) - HK + 1)
-    new_W = (WI + (2 * pad_W) - WK + 1)
+    # we get the new shape of the conv matrix using these equations
+    new_H = (ih + (2 * ph) - kh + 1)
+    new_W = (iw + (2 * pw) - kw + 1)
 
     conv = np.zeros((m, new_H, new_W))
     for i in range(new_H):
         for j in range(new_W):
-            image_part = new_images[:, i:i + HK, j:j + WK]
+            image_part = new_images[:, i:i + kh, j:j + kw]
             result = image_part * kernel
             result = result.sum(axis=1)
             result = result.sum(axis=1)

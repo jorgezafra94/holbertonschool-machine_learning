8cab666d05a357c5dca21727f5ef5ef3e059b38b
jorgezafra94 <947@holbertonschool.com>
2020-06-04 21:24:39 +0000

    [fe] applying filter and padding to images

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -18,8 +18,14 @@ def convolve_grayscale_same(images, kernel):
     """
     kh, kw = kernel.shape
     # when we have to give a padding we have to calculate it like this
-    ph = kh // 2
-    pw = kw // 2
+    if kh % 2 == 0:
+        ph = int(kh / 2)
+    else:
+        ph = int((kh - 1) / 2)
+    if kw % 2 == 0:
+        pw = int(kw / 2)
+    else:
+        pw = int((kw - 1) / 2)
     # We have to apply the padding to the input images
     new_images = np.pad(images,
                         ((0, 0), (ph, ph), (pw, pw)),
@@ -27,8 +33,8 @@ def convolve_grayscale_same(images, kernel):
     m, ih, iw = images.shape
 
     # we get the new shape of the conv matrix using these equations
-    new_H = (ih + (2 * ph) - kh + 1)
-    new_W = (iw + (2 * pw) - kw + 1)
+    new_H = int(ih + (2 * ph) - kh + 1)
+    new_W = int(iw + (2 * pw) - kw + 1)
 
     conv = np.zeros((m, new_H, new_W))
     for i in range(new_H):

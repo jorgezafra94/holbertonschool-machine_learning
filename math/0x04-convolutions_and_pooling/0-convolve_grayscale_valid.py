86e1effe63ed62c8f05d72c3f8dbaf3518ce57ea
jorgezafra94 <947@holbertonschool.com>
2020-06-04 01:09:03 +0000

    [fe] applying only filter

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -14,17 +14,25 @@ def convolve_grayscale_valid(images, kernel):
 
     Returns:
     """
-    m, HI, WI = images.shape
-    HK, WK = kernel.shape
-    new_H = (HI - HK + 1)
-    new_W = (WI - WK + 1)
+    m, ih, iw = images.shape
+    kh, kw = kernel.shape
+    new_H = (ih - kh + 1)
+    new_W = (ih - kw + 1)
 
     conv = np.zeros((m, new_H, new_W))
     for i in range(new_H):
         for j in range(new_W):
-            image_part = images[:, i:i+HK, j:j+WK]
+            # we get here same the part for each one of the images
+            image_part = images[:, i:i+kh, j:j+kw]
+            # we get here the result of multiplication
             result = image_part * kernel
+            # we have to get a number from result (matrix of matrices)
             result = result.sum(axis=1)
             result = result.sum(axis=1)
+            # once we get the numbers we are going to
+            # store the numbers in the same position for
+            # each image
             conv[:, i, j] = result
+            # every time we go through the loop we are getting
+            # one position of the conv for each one of the images
     return conv

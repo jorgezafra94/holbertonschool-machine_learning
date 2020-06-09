5f6c49e4e9de5a826582c7a8ede5f3a6b2698af7
jorgezafra94 <947@holbertonschool.com>
2020-06-09 11:47:35 -0500

    [fe] backpropagation with Pooling

--- supervised_learning/0x07-cnn/3-pool_backward.py
+++ supervised_learning/0x07-cnn/3-pool_backward.py
@@ -30,3 +30,28 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
     Returns: the partial derivatives with respect to the
     previous layer (dA_prev)
     """
+    m, h_new, w_new, c_new = dA.shape
+    m, h_prev, w_prev, c = A_prev.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
+
+    # creating derivatives
+    dA_prev = np.zeros(A_prev.shape)
+
+    for elem in range(m):
+        im = A_prev[elem]
+        for i in range(h_new):
+            for j in range(w_new):
+                for f in range(c_new):
+                    sth = i * sh
+                    endh = (i * sh) + kh
+                    stw = j * sw
+                    endw = (j * sw) + kw
+                    X = im[sth:endh, stw:endw, f]
+                    if mode == 'max':
+                        mask = np.where(X == np.max(X), 1, 0)
+                    if mode == 'avg':
+                        mask = np.ones(X.shape)
+                    aux = mask * dA[elem, i, j, f]
+                    dA_prev[elem, sth:endh, stw:endw, f] = aux
+    return dA_prev

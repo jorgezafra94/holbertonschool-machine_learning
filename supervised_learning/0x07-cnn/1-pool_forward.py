2675fa45bcb842c7e3a9820a08512752f3d5896f
jorgezafra94 <947@holbertonschool.com>
2020-06-08 19:14:20 -0500

    [fe] forward propagation pooling

--- supervised_learning/0x07-cnn/1-pool_forward.py
+++ supervised_learning/0x07-cnn/1-pool_forward.py
@@ -25,3 +25,25 @@ def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
       to perform maximum or average pooling, respectively
     Returns: the output of the pooling layer
     """
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw= kernel_shape
+    sh, sw = stride
+
+    # dims conv matrix
+    pool_h = int(((h_prev - kh) / sh) + 1)
+    pool_w = int(((w_prev - kw) / sw) + 1)
+    pool = np.zeros((m, pool_h, pool_w, c_prev))
+
+    for i in range(pool_h):
+        for j in range(pool_w):
+                sth = i * sh
+                endh = (i * sh) + kh
+                stw = j * sw
+                endw = (j * sw) + kw
+                X = A_prev[:, sth:endh, stw:endw]
+                if mode == "max":
+                    WX = np.max(X, axis=(1, 2))
+                if mode == "avg":
+                    WX = np.mean(X, axis=(1, 2))
+                pool[:, i, j] = WX
+    return pool

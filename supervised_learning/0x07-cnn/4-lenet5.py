44ce2a1b9c29aab89b53e24199868bf5bb7247ad
jorgezafra94 <947@holbertonschool.com>
2020-06-09 15:51:15 -0500

    [fe] lenet arquitecture with Tensorflow

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -35,3 +35,56 @@ def lenet5(x, y):
     - a tensor for the loss of the netowrk
     - a tensor for the accuracy of the network
     """
+    lay_init = tf.contrib.layers.variance_scaling_initializer()
+    act_f = tf.nn.relu
+    # Convolutional layer with 6 kernels of shape 5x5 with same padding
+    conv_layer1 = tf.layers.Conv2D(filters=6, kernel_size=(5, 5),
+                                   padding='same', activation=act_f,
+                                   kernel_initializer=lay_init)
+    conv1 = conv_layer1(x)
+
+    # Max pooling layer with kernels of shape 2x2 with 2x2 strides
+    pool_layer1 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))
+    pool1 = pool_layer1(conv1)
+
+    # Convolutional layer with 16 kernels of shape 5x5 with valid padding
+    conv_layer2 = tf.layers.Conv2D(filters=16, kernel_size=(5, 5),
+                                   padding='valid', activation=act_f,
+                                   kernel_initializer=lay_init)
+    conv2 = conv_layer2(pool1)
+
+    # Max pooling layer with kernels of shape 2x2 with 2x2 strides
+    pool_layer2 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))
+    pool2 = pool_layer2(conv2)
+
+    # change form matrix to vector
+    x_vec = tf.layers.Flatten()(pool2)
+
+    # Fully connected layer with 120 nodes
+    layer3 = tf.layers.Dense(units=120, activation=act_f,
+                             kernel_initializer=lay_init)
+    FC3 = layer3(x_vec)
+
+    # Fully connected layer with 84 nodes
+    layer4 = tf.layers.Dense(units=84, activation=act_f,
+                             kernel_initializer=lay_init)
+    FC4 = layer4(FC3)
+
+    # Fully connected softmax output layer with 10 nodes
+    layer5 = tf.layers.Dense(units=10, kernel_initializer=lay_init)
+    y_pred = layer5(FC4)
+    # loss
+    loss = tf.losses.softmax_cross_entropy(y, y_pred)
+
+    # accuracy
+    index_y = tf.math.argmax(y, axis=1)
+    index_pred = tf.math.argmax(y_pred, axis=1)
+    comp = tf.math.equal(index_y, index_pred)
+    cast = tf.cast(comp, dtype=tf.float32)
+    accuracy = tf.math.reduce_mean(cast)
+
+    # train with Adam optimizer
+    Adam = tf.train.AdamOptimizer().minimize(loss)
+
+    y_softmax = tf.nn.softmax(y_pred)
+    return (y_softmax, Adam, loss, accuracy)

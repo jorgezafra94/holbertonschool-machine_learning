11eb2d0ac6679ceccdb220055d5b60352e0f346e
jorgezafra94 <947@holbertonschool.com>
2020-07-06 20:03:38 -0500

    [fe] creating method to get landmarks in the face

--- supervised_learning/0x0B-face_verification/align.py
+++ supervised_learning/0x0B-face_verification/align.py
@@ -5,6 +5,7 @@ Face Align
 
 import dlib
 import cv2
+import numpy as np
 
 
 class FaceAlign:
@@ -37,11 +38,10 @@ class FaceAlign:
         try:
             # we have to change the image to gray scale
             img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
             # we get the faces in the gray picture
-            # The 1 in the second argument indicates that we should upsample the image
-            # 1 time.  This will make everything bigger and allow us to detect more
-            # faces.
+            # The 1 in the second argument indicates that we should
+            # upsample the image 1 time.  This will make everything
+            # bigger and allow us to detect more faces.
             faces = self.detector(img_gray, 1)
             # we have to go through all the faces
             box = [0, 0, image.shape[1], image.shape[0]]
@@ -58,6 +58,32 @@ class FaceAlign:
 
             box_coordinates = tuple(box)
             return(dlib.rectangle(*box_coordinates))
+
+        except Exception:
+            return None
+
+    def find_landmarks(self, image, detection):
+        """
+         getting facial landmarks
+        * image is a numpy.ndarray of an image from which to find facial
+            landmarks
+        * detection is a dlib.rectangle containing the boundary box of
+            the face in the image
+        Returns: a numpy.ndarray of shape (p, 2)containing the landmark
+            points, or None on failure
+        * p is the number of landmark points
+        * 2 is the x and y coordinates of the point
+        """
+        try:
+            img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            parts = self.shape_predictor(img_gray, detection)
+            landmarks = np.zeros((parts.num_parts, 2), dtype=np.int)
+
+            for i in range(len(parts.num_parts)):
+                landmarks[i, 0] = parts.num_parts(i).x
+                landmarks[i, 1] = parts.num_parts(i).y
+
+            return landmarks
 
         except Exception:
             return None

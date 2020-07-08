006b5b8f4a287bd7fee376d05767cdf3db0fa590
jorgezafra94 <947@holbertonschool.com>
2020-07-08 13:52:22 -0500

    [fe] creating model for train

--- supervised_learning/0x0B-face_verification/train_model.py
+++ supervised_learning/0x0B-face_verification/train_model.py
@@ -1,32 +1,60 @@
 #!/usr/bin/env python3
-"""contains the TrainModel class"""
+"""
+contains the TrainModel class
+"""
+
 from triplet_loss import TripletLoss
 import tensorflow as tf
+
+
 class TrainModel:
     """
     TrainModel class
     """
     def __init__(self, model_path, alpha):
         """
-        constructor
-        :param model_path: path to the base face verification embedding model
-            loads the model using with tf.keras.utils.CustomObjectScope({'tf': tf}):
-            saves this model as the public instance method base_model
-        :param alpha: alpha to use for the triplet loss calculation
+        * model_path is the path to the base face verification
+          embedding model
+        * loads the model using with
+          tf.keras.utils.CustomObjectScope({'tf': tf}):
+        * saves this model as the public instance method base_model
+        * alpha is the alpha to use for the triplet loss calculation
+        * Creates a new model:
+        * * inputs: [A, P, N]
+            - A is a numpy.ndarray of shape (m, n, n, 3)containing the
+              aligned anchor images
+            - P is a numpy.ndarray of shape (m, n, n, 3) containing the
+              aligned positive images
+            - N is a numpy.ndarray of shape (m, n, n, 3)containing the
+              aligned negative images
+            - m is the number of images
+            - n is the size of the aligned images
+        * * outputs: the triplet losses of base_model
+        * * compiles the model with:
+            - Adam optimization
+            - no additional losses
+        * * save this model as the public instance attribute training_model
+        * you can use from triplet_loss import TripletLoss
         """
-        """with tf.keras.utils.CustomObjectScope({'tf': tf}):"""
-        self.training_model = tf.keras.models.load_model(model_path)
-        """self.base_model.save('base_model')
-        A = tf.placeholder(tf.float32, (None, 96, 96, 3))
-        P = tf.placeholder(tf.float32, (None, 96, 96, 3))
-        N = tf.placeholder(tf.float32, (None, 96, 96, 3))
-        inputs = [A, P, N]
-        output = self.base_model(inputs)
+        with tf.keras.utils.CustomObjectScope({'tf': tf}):
+            self.base_model = tf.keras.models.load_model(model_path)
+
+        # complete Model
+        # we create this Inputs because there are the inputs
+        # of base_model model
+        A = tf.keras.Input(shape=(96, 96, 3))
+        P = tf.keras.Input(shape=(96, 96, 3))
+        N = tf.keras.Input(shape=(96, 96, 3))
+        predict_a = self.base_model(A)
+        predict_b = self.base_model(P)
+        predict_c = self.base_model(N)
         tl = TripletLoss(alpha)
-        output = tl(output)
-        self.training_model = tf.keras.models.Model(inputs, output)
-        self.training_model.compile(optimizer='Adam')
-        self.training_model.save('training_model')"""
+        output = tl([predict_a, predict_b, predict_c])
+        model_fin = tf.keras.models.Model([A, P, N], outputs=output)
+        model_fin.compile(optimizer='adam')
+        self.training_model = model_fin
+
+
 
 
 

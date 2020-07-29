7e6cf549c24335a47b5f31c594a6ec6a07c42ba9
jorgezafra94 <947@holbertonschool.com>
2020-07-29 09:32:34 -0500

    [fix] revert change

--- unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
+++ unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
@@ -4,6 +4,8 @@ t-SNE (stochastic Neightbor Embedding)
 """
 
 import numpy as np
+P_init = __import__('2-P_init').P_init
+HP = __import__('3-entropy').HP
 
 
 def P_affinities(X, tol=1e-5, perplexity=30.0):
@@ -21,8 +23,6 @@ def P_affinities(X, tol=1e-5, perplexity=30.0):
     Returns: P, a numpy.ndarray of shape (n, n) containing the symmetric
     P affinities
     """
-    P_init = __import__('2-P_init').P_init
-    HP = __import__('3-entropy').HP
     n, d = X.shape
     D, P, betas, H = P_init(X, perplexity)
     for i in range(n):

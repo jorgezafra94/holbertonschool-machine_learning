3a370f750f16be4e3a5b225bd279c36e0605af68
jorgezafra94 <947@holbertonschool.com>
2020-07-29 14:33:30 -0500

    [fix] change the way of code aux

--- unsupervised_learning/0x00-dimensionality_reduction/6-grads.py
+++ unsupervised_learning/0x00-dimensionality_reduction/6-grads.py
@@ -29,11 +29,12 @@ def grads(Y, P):
     dY = np.zeros((n, ndim))
     # *************************** step by step **************
     # for i in range(n):
-        # aux = np.tile(equation1[:, i].reshape(-1, 1), 2)
-        # dY[i] = (aux * (Y[i] - Y)).sum(axis=0)
+    #    aux = np.tile(equation1[:, i].reshape(-1, 1), 2)
+    #    dY[i] = (aux * (Y[i] - Y)).sum(axis=0)
     for i in range(n):
         # we have to reshape aux because it is (2500,) shape
-        aux = np.tile(equation1[:, i], (ndim, 1)).T
+        #aux1 = np.tile(equation1[:, i], (ndim, 1)).T
+        aux = np.tile(equation1[:, i].reshape(-1, 1), 2)
         # after this process we get a shape (2500, 2) now we can
         # do the multiplication
         dY[i] = (aux * (Y[i] - Y)).sum(axis=0)

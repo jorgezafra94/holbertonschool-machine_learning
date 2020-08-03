88b4c6c17156e8e81286e1a38d0c256d1d3defe7
jorgezafra94 <947@holbertonschool.com>
2020-08-03 14:16:32 -0500

    [fe] initialize centroids using multivariate uniform distribution

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -24,7 +24,7 @@ def initialize(X, k):
     * Returns: a numpy.ndarray of shape (k, d) containing the initialized
       centroids for each cluster, or None on failure
     """
-    if type(X) is not np.ndarray:
+    if type(X) is not np.ndarray or len(X.shape) != 2:
         return None
 
     if type(k) is not int or k <= 0:

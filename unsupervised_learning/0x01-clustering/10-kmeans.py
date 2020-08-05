a7c2ecc4dc0756956a71ea91329a032cec386bff
jorgezafra94 <947@holbertonschool.com>
2020-08-04 19:02:00 -0500

    [fe] getting C and classes using scikit learn

---
+++ unsupervised_learning/0x01-clustering/10-kmeans.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+kmeans using scikit-learn
+"""
+
+import sklearn.cluster
+
+
+def kmeans(X, k):
+    """
+    * X is a numpy.ndarray of shape (n, d) containing the dataset
+    * k is the number of clusters
+
+    Returns: C, clss
+    * C is a numpy.ndarray of shape (k, d) containing the centroid
+      means for each cluster
+    * clss is a numpy.ndarray of shape (n,) containing the index of
+      the cluster in C that each data point belongs to
+    """
+    kmeans = sklearn.cluster.KMeans(n_clusters=k, random_state=0).fit(X)
+
+    C = kmeans.cluster_centers_
+    clss = kmeans.labels_
+
+    return C, clss

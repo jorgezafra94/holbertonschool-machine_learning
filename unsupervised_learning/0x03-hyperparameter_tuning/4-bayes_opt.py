face521e3f72f94fb5d403e8385073724dd2cd24
jorgezafra94 <947@holbertonschool.com>
2020-08-18 19:26:51 -0500

    [fix] adding for loop to ei

--- unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
@@ -78,8 +78,12 @@ class BayesianOptimization():
             else:
                 Z[i] = 0
 
-        ei = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
-        ei[sigma == 0.0] = 0.0
+        ei = np.zeros(sigma.shape)
+        for i in range(len(sigma)):
+            if sigma[i] > 0:
+                ei[i] = imp[i] * norm.cdf(Z[i]) + sigma[i] * norm.pdf(Z[i])
+            else:
+                ei[i] = 0
 
         index = np.argmax(ei)
         best_sample = self.X_s[index]

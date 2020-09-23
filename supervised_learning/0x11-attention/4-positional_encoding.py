eb3aa4829086e038638fbc28bdff86891a2cc701
jorgezafra94 <947@holbertonschool.com>
2020-09-23 16:20:05 -0500

    [fe] Positional Encoding

---
+++ supervised_learning/0x11-attention/4-positional_encoding.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+get it from:
+https://datascience.stackexchange.com/questions
+/51065/what-is-the-positional-encoding-in-the-transformer-model
+Positional Encoding
+"""
+
+import numpy as np
+
+
+def positional_encoding(max_seq_len, dm):
+    """
+    max_seq_len is an integer representing the maximum sequence length
+    dm is the model depth
+
+    Returns: a numpy.ndarray of shape (max_seq_len, dm) containing the
+    positional encoding vectors
+    """
+    result = np.zeros((max_seq_len, dm))
+
+    for i in range(max_seq_len):
+        for j in range(0, dm, 2):
+            div_term = np.exp(j * -np.log(10000.0) / dm)
+            result[i, j] = np.sin(i * div_term)
+            result[i, j + 1] = np.cos(i * div_term)
+
+    return result

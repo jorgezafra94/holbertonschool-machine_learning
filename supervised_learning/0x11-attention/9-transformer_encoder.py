a6dc4bd058a9990efa429e52dc4addc38379a142
jorgezafra94 <947@holbertonschool.com>
2020-09-24 17:38:45 -0500

    [fix] Encoder - adding positional_new

--- supervised_learning/0x11-attention/9-transformer_encoder.py
+++ supervised_learning/0x11-attention/9-transformer_encoder.py
@@ -7,14 +7,12 @@ remember to use tensorflow 1.15 or higher
 """
 
 import tensorflow as tf
+
 positional_encoding = __import__('4-positional_encoding').positional_encoding
 EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
 
 
 class Encoder(tf.keras.layers.Layer):
-    """
-    Encoder class
-    """
 
     def __init__(self, N, dm, h, hidden, input_vocab,
                  max_seq_len, drop_rate=0.1):

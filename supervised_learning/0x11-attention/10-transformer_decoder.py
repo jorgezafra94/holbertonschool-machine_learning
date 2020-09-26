3956c8b924ed335b750ae8d80fa2829a8d50b5f9
jorgezafra94 <947@holbertonschool.com>
2020-09-25 19:23:19 -0500

    [fe] Decoder, Union DecoderBlock and positional_enc

--- supervised_learning/0x11-attention/10-transformer_decoder.py
+++ supervised_learning/0x11-attention/10-transformer_decoder.py
@@ -64,7 +64,7 @@ class Decoder(tf.keras.layers.Layer):
         Returns: a tensor of shape (batch, target_seq_len, dm)
         containing the decoder output
         """
-        seq_len = tf.shape(x)[1]
+        seq_len = x.shape[1]
 
         x = self.embedding(x)
         x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))

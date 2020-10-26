2787a6b451b20487c1b4148d7e5ea97af3f65c36
jorgezafra94 <947@holbertonschool.com>
2020-10-26 10:42:30 -0500

    [feature] create a Dataframe from a dictionary

---
+++ pipeline/0x00-pandas/1-from_dict.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Convert a Numpy to a Pandas
+"""
+
+import pandas as pd
+
+dict = {
+    'First': [0.0, 0.5, 1.0, 1.5],
+    'Second': ['one', 'two', 'three', 'four']
+}
+
+df = pd.DataFrame(dict, index=['A', 'B', 'C', 'D'])

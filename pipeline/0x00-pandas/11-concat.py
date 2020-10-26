f1d887ef43d2eb736fad367db21f7768dcf060a9
jorgezafra94 <947@holbertonschool.com>
2020-10-26 18:16:44 -0500

    [feature] using concat and keys with pandas

--- pipeline/0x00-pandas/11-concat.py
+++ pipeline/0x00-pandas/11-concat.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-
+"""
+using concat to join two dataframes
+"""
 import pandas as pd
 from_file = __import__('2-from_file').from_file
 
@@ -7,12 +9,11 @@ df1 = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 df2 = from_file('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', ',')
 
 df2 = df2.sort_values(by='Timestamp', ascending=True)
-df2 = df2[df2['Timestamp'] <= 1417411980]
+df2 = df2[df2['Timestamp'] <= 1417411920]
 
 df1 = df1.set_index('Timestamp')
 df2 = df2.set_index('Timestamp')
 
 df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
-
 
 print(df)

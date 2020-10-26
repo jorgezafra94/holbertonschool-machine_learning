580aea7b6a230187fb257e785d09238d52794d4f
jorgezafra94 <947@holbertonschool.com>
2020-10-26 16:34:19 -0500

    [feature] using describe to get stats

---
+++ pipeline/0x00-pandas/13-analyze.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+getting stats from dataframe
+using knowledge DS4A
+"""
+
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+
+stats = df.loc[:, df.columns != 'Timestamp'].describe()
+
+print(stats)

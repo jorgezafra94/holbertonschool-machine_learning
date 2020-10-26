d204d29f2124f821527b6199708bb65869b35759
jorgezafra94 <947@holbertonschool.com>
2020-10-26 14:41:08 -0500

    [update] adding set_option to print as the intranet want using tranpose

--- pipeline/0x00-pandas/6-flip_switch.py
+++ pipeline/0x00-pandas/6-flip_switch.py
@@ -8,7 +8,9 @@ import pandas as pd
 from_file = __import__('2-from_file').from_file
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-pd.reset_option('display.max_rows')
+
+pd.set_option('display.max_columns', 6)
+
 df = df.sort_values(by=['Timestamp'], ascending=False).transpose()
 
 print(df.tail())

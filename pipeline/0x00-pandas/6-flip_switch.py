39e4627fc7865c6fc0914edf9e151f72fe8da57b
jorgezafra94 <947@holbertonschool.com>
2020-10-26 14:20:30 -0500

    [feature] transpose in pandas

---
+++ pipeline/0x00-pandas/6-flip_switch.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+transpose dataframe and sort_values
+using DS4A knowledge
+"""
+import pandas as pd
+
+from_file = __import__('2-from_file').from_file
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+pd.reset_option('display.max_rows')
+df = df.sort_values(by=['Timestamp'], ascending=False).transpose()
+
+print(df.tail())

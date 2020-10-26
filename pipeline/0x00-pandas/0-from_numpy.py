443a6a33cdf2b34a28e71d6ef6207f0b5843abbd
jorgezafra94 <947@holbertonschool.com>
2020-10-26 10:08:04 -0500

    [feature] convert a numpy to Dataframe

---
+++ pipeline/0x00-pandas/0-from_numpy.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Convert a Numpy to a Pandas
+"""
+
+import pandas as pd
+
+
+def from_numpy(array):
+    """
+    * array: numpy array from which you should create the Dataframe
+    """
+    pd.set_option('display.max_columns', None)
+    my_pandas = pd.DataFrame(array)
+    # control more than 26 columns we restrict
+    if my_pandas.shape[1] > 26:
+        my_pandas = my_pandas[:26]
+    # giving column names alphabetical order and capitalized
+    cols_name = []
+    for i in range(len(my_pandas.columns.tolist())):
+        a = '{}'.format(chr(i + 65))
+        cols_name.append(a)
+    my_pandas.columns = cols_name
+    return my_pandas

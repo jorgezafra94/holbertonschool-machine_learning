9681d57e2a8fc9588a8459a2af0406f888a6dd65
jorgezafra94 <947@holbertonschool.com>
2020-10-28 10:34:54 -0500

    [feature] consumin Github API

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
 """ script that prints the location of a specific github user"""
-
-import sys
 import requests
+import sys
 import time
 
+
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        header = {'Accept': 'application/vnd.github.v3+json'}
-        url = sys.argv[1]
+    argv = sys.argv
+    argc = len(argv)
+    if argc > 1:
+        page_url = argv[1]
 
-        data = requests.get(url, params=header)
-
-        if data.status_code == 403:
-            reset = data.headers['X-Ratelimit-Reset']
+        res = requests.get(page_url)
+        status_code = res.status_code
+        if status_code == 200:
+            print(res.json()['location'])
+        elif status_code == 404:
+            print('Not found')
+        elif status_code == 403:
+            reset = res.headers['X-Ratelimit-Reset']
             X = int((int(reset) - int(time.time())) / 60)
             print("Reset in {} min".format(X))
-
-        elif data.status_code == 200:
-            response = data.json()
-            print(response['location'])
-
-        elif data.status_code == 404:
-            print('Not found')

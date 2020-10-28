f36081d9312b3f93dfd2c7102c410db764e213c8
jorgezafra94 <947@holbertonschool.com>
2020-10-28 10:41:55 -0500

    [fe] github api

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Retrieving user location
+Consuming API github
 """
 import requests
 import sys
@@ -8,19 +8,17 @@ import time
 
 if __name__ == '__main__':
     url = sys.argv[1]
-
     headers = {'Accept': 'application/vnd.github.v3+json'}
-    r = requests.get(url, headers=headers)
+    data = requests.get(url, headers=headers)
 
-    if r.status_code == 200:
+    if data.status_code == 200:
+        print(data.json()['location'])
 
-        print(r.json()['location'])
-
-    elif r.status_code == 404:
+    if data.status_code == 404:
         print("Not found")
 
-    elif r.status_code == 403:
-        limit_time = int(r.headers['X-Ratelimit-Reset'])
+    if data.status_code == 403:
+        limit_time = int(data.headers['X-Ratelimit-Reset'])
         now = int(time.time())
-        remaining = int((limit_time - now) / 60)
-        print("Reset in {} min".format(remaining))
+        result = int((limit_time - now) / 60)
+        print("Reset in {} min".format(result))

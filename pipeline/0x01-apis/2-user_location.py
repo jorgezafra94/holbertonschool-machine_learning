383cf24e69fcba750e9d360bf9781a590bc2912c
jorgezafra94 <947@holbertonschool.com>
2020-10-28 10:31:38 -0500

    [feature] consumin Github API

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -8,20 +8,21 @@ import requests
 import time
 
 if __name__ == '__main__':
-    header = {'Accept': 'application/vnd.github.v3+json'}
-    url = sys.argv[1]
+    if len(sys.argv) > 1:
+        header = {'Accept': 'application/vnd.github.v3+json'}
+        url = sys.argv[1]
 
-    data = requests.get(url, params=header)
+        data = requests.get(url, params=header)
 
-    if data.status_code == 200:
-        response = data.json()
-        print(response['location'])
+        if data.status_code == 403:
+            ratelimit = data.headers['X-Ratelimit-Reset']
+            now = time.time()
+            result = int((int(ratelimit) - now) / 60)
+            print('Reset in {} min'.format(result))
 
-    elif data.status_code == 404:
-        print('Not found')
+        elif data.status_code == 200:
+            response = data.json()
+            print(response['location'])
 
-    elif data.status_code == 403:
-        ratelimit = data.headers['X-Ratelimit-Reset']
-        now = time.time()
-        result = int((int(ratelimit) - int(now)) / 60)
-        print('Reset in {} min'.format(result))
+        elif data.status_code == 404:
+            print('Not found')

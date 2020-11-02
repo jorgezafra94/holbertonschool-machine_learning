2034b1f99bc584fc4c54b33f799396b4b45dd111
jorgezafra94 <jorgezafra94@hotmail.com>
2020-11-02 20:36:19 +0000

    [fe] trigger in sql

--- pipeline/0x02-databases/17-store.sql
+++ pipeline/0x02-databases/17-store.sql
@@ -3,13 +3,13 @@
 -- the  idea is to use the name to know what quantity we should decrease
 -- so here if a new order is created the number of the elements in the order will be decreased in the items table
 -- like an inventory if you sell items you have to decrease the number of elements that you have
-DELIMITER //
+DELIMITER $$
 CREATE TRIGGER items_updated
    AFTER INSERT
    ON orders FOR EACH ROW
 BEGIN
    UPDATE items SET quantity = quantity - NEW.number
    WHERE items.name = NEW.item_name;
-END //
+END $$
 
 DELIMITER ;

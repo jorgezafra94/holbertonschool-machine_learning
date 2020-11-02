57da97daab19fc6cfd0e85cb9e750797c5feac25
jorgezafra94 <jorgezafra94@hotmail.com>
2020-11-02 16:02:51 +0000

    [fe] using max orderby and groupby mysql

--- pipeline/0x02-databases/7-max_state.sql
+++ pipeline/0x02-databases/7-max_state.sql
@@ -1,4 +1,4 @@
 -- getting max value of each state in temperature table
-SELECT state, MAX(value) from temperatures
+SELECT state, MAX(value) AS max_temp from temperatures
 GROUP BY state
 ORDER BY state;

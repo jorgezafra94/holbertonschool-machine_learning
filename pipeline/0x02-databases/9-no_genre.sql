82135b46a0b2467853dd0ba40491141c021cbf0b
jorgezafra94 <jorgezafra94@hotmail.com>
2020-11-02 16:48:55 +0000

    [fe] get NULL genres_id

---
+++ pipeline/0x02-databases/9-no_genre.sql
@@ -0,0 +1,5 @@
+-- getting NULL gender_id
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE genre_id is NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;

a86842b0b144dc102be2c40413fa347e04ebf54c
jorgezafra94 <jorgezafra94@hotmail.com>
2020-11-02 17:43:38 +0000

    [fe] sum of ratings

---
+++ pipeline/0x02-databases/11-rating_shows.sql
@@ -0,0 +1,6 @@
+-- remember always create the database
+-- then you can mysql -u root -p hbtn_0d_tvshows_rate < ./hbtn_0d_tvshows_rate.sql
+SELECT tv_shows.title AS title, SUM(tv_show_ratings.rate) AS rating FROM tv_shows
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY title
+ORDER BY rating DESC; 

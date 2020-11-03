c04a4b97344e8e764696ea30d60d28b115a5b513
jorgezafra94 <jorgezafra94@hotmail.com>
2020-11-03 19:16:33 +0000

    [fe] create a view in mysql

--- pipeline/0x02-databases/102-need_meeting.sql
+++ pipeline/0x02-databases/102-need_meeting.sql
@@ -1,4 +1,5 @@
 -- creating a view in mysql
+-- https://www.mysqltutorial.org/create-sql-views-mysql.aspx#:~:text=%20MySQL%20CREATE%20VIEW%20%201%20Creating%20a,VIEW%20statement%20to%20create%20a%20view...%20More%20
 CREATE VIEW need_meeting AS
     SELECT 
         name

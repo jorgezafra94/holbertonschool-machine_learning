6a3990f9c6a3387acc62a80ca1daa405f1157f6a
jorgezafra94 <jorgezafra94@hotmail.com>
2020-11-04 10:01:22 -0500

    [fe] using pipelines in Pymongo

--- pipeline/0x02-databases/106-log_stats.py
+++ pipeline/0x02-databases/106-log_stats.py
@@ -1,42 +1,40 @@
 #!/usr/bin/env python3
 """
-Log stats using pymongo
-https://docs.mongodb.com/manual/core/aggregation-pipeline-optimization/
+nginx logs
 """
-
 from pymongo import MongoClient
 
-
-if __name__ == "__main__":  
+if __name__ == "__main__":
     client = MongoClient()
+    collection = client.logs.nginx
 
-    collection_logs = client.logs.nginx
+    # cursor = collection.find({})
+    # print(cursor[0])
 
-    num_logs = collection_logs.count_documents({})
-    
-    print("{} logs".format(num_logs))
+    number_logs = collection.count_documents({})
+    print("{} logs".format(number_logs))
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
     print("Methods:")
-    
-    for elem in methods:
-        docs_method = collection_logs.count_documents({'method': elem})
-        print("\tmethod {}: {}".format(elem, docs_method))
+    for method in methods:
+        query = {"method": method}
+        count_methods = collection.count_documents(query)
+        print("\tmethod {}: {}".format(method, count_methods))
 
-    my_query= {"method": "GET", "path": "/status"}
-    status_check = collection_logs.count_documents(my_query)
+    query2 = {"method": "GET", "path": "/status"}
+    count2 = collection.count_documents(query2)
+    print("{} status check".format(count2))
 
-    print("{} status check".format(status_check))
-
-    print('IPs:')
+    print("IPs:")
 
     pipeline = [
         {"$sortByCount": '$ip'},
         {"$limit": 10},
         {"$sort": {"ip": -1}},
+
     ]
     ips = collection.aggregate(pipeline)
 
     for ip in ips:
         print("\t{}: {}".format(ip['_id'], ip['count']))
+        

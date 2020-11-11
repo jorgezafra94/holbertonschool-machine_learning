03bd18e5ab1ae94444a93fcae73b47e6e30bca0f
jorgezafra94 <jorgezafra94@hotmail.com>
2020-11-11 16:49:44 -0500

    [fe] flip 3d image tensorflow

---
+++ pipeline/0x03-data_augmentation/1-crop.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Crop an image with tensorflow
+"""
+import tensorflow as tf
+
+
+def crop_image(image, size):
+    """
+    * image is a 3D tf.Tensor containing the image to crop
+    * size is a tuple containing the size of the crop
+    Returns the cropped image
+    """
+    img = tf.random_crop(value=image, size=size)
+    return img

5c4b11dd429d616a8e983d07f6191abcc64782c2
jorgezafra94 <jorgezafra94@hotmail.com>
2020-11-11 12:19:19 -0500

    files functions

---
+++ pipeline/0x03-data_augmentation/0-flip.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+How to Flip an image with tensorflow
+"""
+import tensorflow as tf
+
+
+def flip_image(image):
+    """
+    * image is a 3D tf.Tensor containing the image to flip
+    * Returns the flipped image
+    """
+    flip = tf.image.flip_left_right(image)
+    return flip

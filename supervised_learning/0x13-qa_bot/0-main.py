cecc9456167aa5d83000b78a5ec3ea9a2cb24bee
jorgezafra94 <jorgezafra94@hotmail.com>
2020-11-24 11:53:37 -0500

    main point 0

---
+++ supervised_learning/0x13-qa_bot/0-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+question_answer = __import__('0-qa').question_answer
+
+with open('ZendeskArticles/PeerLearningDays.md') as f:
+    reference = f.read()
+
+print(question_answer('When are PLDs?', reference))
+print(question_answer('Cuantos años tengo', reference))
